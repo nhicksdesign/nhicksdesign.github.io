5580b95a7a3930f0bcea3a74a20fcb75ede2b7e2
Nate Hicks <nhicksdesign@gmail.com>
2017-10-01 18:06:33 -0400

    All the Updates

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,39 +1,75 @@
-window.onscroll = function () {
-    scrollFunction();
-};
+$(document).ready(function () {
 
-function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
-    document.documentElement.scrollTop = 0; // For IE and Firefox
-}
-
-var $contactForm = $('#contact-form');
-$contactForm.submit(function (e) {
-    e.preventDefault();
-    $.ajax({
-        url: '//formspree.io/nhicksdesign@gmail.com',
-        method: 'POST',
-        data: $(this).serialize(),
-        dataType: 'json',
-        beforeSend: function () {
-            $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
-        },
-        success: function (data) {
-            $contactForm.find('.alert--loading').hide();
-            $contactForm.append('<div class="alert alert--success">Message sent!</div>');
-        },
-        error: function (err) {
-            $contactForm.find('.alert--loading').hide();
-            $contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+    /*STICKY NAVIGATION*/
+    $("#about").waypoint(function (direction) {
+        if (direction == 'down') {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
         }
+    }, {
+        offset: '60px;'
+    });
+
+    /*SCROLL TO*/
+    $('.home-button').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.hero-text-box').offset().top
+        }, 1000);
+    });
+
+    $('.about-button').click(function () {
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 1000);
+    });
+
+    $('.work-button').click(function () {
+        $('html, body').animate({
+            scrollTop: $("#work").offset().top
+        }, 1000);
+    });
+
+    $('.hire-button').click(function () {
+        $('html, body').animate({
+            scrollTop: $("#contact").offset().top
+        }, 1000);
+    });
+
+    /*MOBILE NAVIGATION*/
+    $('.mobile-nav-icon').click(function () {
+        var nav = $('.main-nav');
+        var icon = $('.mobile-nav-icon i');
+
+        nav.slideToggle(200);
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
+
+    var $contactForm = $('#contact-form');
+    $contactForm.submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: '//formspree.io/nhicksdesign@gmail.com',
+            method: 'POST',
+            data: $(this).serialize(),
+            dataType: 'json',
+            beforeSend: function () {
+                $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+            },
+            success: function (data) {
+                $contactForm.find('.alert--loading').hide();
+                $contactForm.append('<div class="alert alert--success">Message sent!</div>');
+            },
+            error: function (err) {
+                $contactForm.find('.alert--loading').hide();
+                $contactForm.append('<div class="alert alert--error">Oops, there was an error.</div>');
+            }
+        });
     });
 });
